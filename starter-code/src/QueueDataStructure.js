9a482fbfcb20cf3c1c46fe05dc98883f50badcbf
paulbourceret <paulbourceret@gmail.com>
2019-06-05 23:49:38 +0200

    done

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,29 @@
-function QueueDataStructure () {
+class QueueDataStructure {
+  constructor() {
+    this.queueControl = [];
+    this.MAX_SIZE = 1000000;
+  }
+  isEmpty() {
+    var res = false;
+    if (this.queueControl.length === 0) return true;
+    else return res;
+  }
+  canEnqueue() {
+    if (this.queueControl.length < this.MAX_SIZE) {
+      return true;
+    } else return false;
+  }
+  enqueue(x) {
+    if (this.MAX_SIZE > this.queueControl + x) {
+      this.queueControl.unshift(x);
+      return this.queueControl;
+    } else {
+      return `Queue Overflow`;
+    }
+  }
+  dequeue() {
+    let last = this.queueControl.pop();
+    if (this.queueControl.length === 0) return `Queue Underflow`;
+    else return last;
+  }
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,33 @@
-function StackDataStructure () {
+class StackDataStructure {
+  constructor() {
+    this.stackControl = [];
+    this.MAX_SIZE = 1000;
+  }
+  isEmpty() {
+    var res = false;
+    if (this.stackControl.length === 0) res = true;
+    return res;
+  }
+  canPush() {
+    var res = false;
+    if (this.stackControl.length < this.MAX_SIZE) res = true;
+    return res;
+  }
+  push(x) {
+    this.stackControl.push(x);
+    this.stackControl;
+    if (x > this.MAX_SIZE - this.stackControl.length) {
+      return `Stack Overflow`;
+    } else {
+      return this.stackControl;
+    }
+  }
+  pop() {
+    let last = this.stackControl.pop();
+    if (this.stackControl.length === 0) {
+      return `Stack Underflow`;
+    } else {
+      return last;
+    }
+  }
 }
